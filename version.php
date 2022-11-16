c14d073cfc3dbc53d9cf675cfa10b95c8a55091e
Paul Reiser <apaulreiser@gmail.com>
2022-11-16 17:27:18 +0100

    Update plugin version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_adaptivemooptnopenalty';
 $plugin->release   = '0.1.1';
-$plugin->version   = 2022090900;
+$plugin->version   = 2022111600;
 $plugin->requires  = 2018112800;
 $plugin->dependencies = array(
     'qbehaviour_adaptivemoopt' => 2022081800
